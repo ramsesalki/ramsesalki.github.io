290991e897b02f1ef3e79710dfdb0721351e378c
Ramses Alki <129827741+ramsesalki@users.noreply.github.com>
2026-07-13 20:01:56 -0600

    Delete proyecto directory

--- proyecto/script/funciones.js
+++
@@ -1,3 +0,0 @@
-function muestraArchivo(htmlDocument) {
-	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
-}
